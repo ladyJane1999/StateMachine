79e28121cfe5eecfcadac7a61e992016d14ed892
ladyJane1999 <anzhela.romanova.99@bk.ru>
2022-09-09 10:35:42 +0300

    Добавьте файлы проекта.

---
+++ StateMachine/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class Program
+{
+    static async Task Main(string[] args)
+    {
+        var taskloop = new TaskLoop()
+        {
+            A = () => Console.WriteLine($"After Delay { Thread.CurrentThread.ManagedThreadId };"),
+            Max = 5
+        };
+        Console.WriteLine($"Hello World { Thread.CurrentThread.ManagedThreadId};");
+        taskloop.Run();
+        taskloop.Task.Wait();
+        Console.ReadKey();
+    }
+
+    }
+    
+
---
+++ StateMachine/TaskLoop.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Runtime.CompilerServices;
+
+internal class TaskLoop : IAsyncStateMachine
+{
+    public Action? A { get; set; }
+    public int Max { get; set; }
+    public Task Task { get; set; }
+
+    private static TaskCompletionSource tcs;
+
+    public Int32 m_state;
+    public Task Run()
+    {
+
+        tcs = new TaskCompletionSource();
+        var awaiterType1 = tcs.Task.GetAwaiter();
+
+        if (!awaiterType1.IsCompleted)
+        {
+            m_state = 0;
+            tcs.Task.ContinueWith(t => MoveNext());   // При завершении Task ContinueWith вызывает MoveNext
+
+        }
+
+        tcs.SetResult();
+        Task= tcs.Task;
+        return tcs.Task;
+    }
+
+    public void MoveNext()
+    {
+        int state = 0;
+        int num = state;
+        try
+        {
+            while (state < Max)
+            {
+                A.Invoke();
+                state++;
+            }            
+        }
+        catch (AggregateException exception)
+        {
+            foreach (Exception ex in exception.InnerExceptions)
+            Console.WriteLine(exception.InnerException);
+            return;
+        }
+           
+    }
+    void IAsyncStateMachine.MoveNext()
+    {
+        MoveNext();
+    }
+
+    void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+    {
+       SetStateMachine(stateMachine);
+    }
+
+    private void SetStateMachine(IAsyncStateMachine stateMachine)
+    {
+    }
+}
